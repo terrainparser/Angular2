ca9f3ad09c0799b98cc587a3f5a4e1110fbd9ac7
terrainparser <harishdotnet6@gmail.com>
2017-02-22 18:09:33 -0500

    service usage syntax change for angular2 final version

--- app/app.module.ts
+++ app/app.module.ts
@@ -6,10 +6,13 @@ import {AuthorsComponent} from './authors.component';
 import {CoursesComponent} from './courses.component';
 import {AutoGrowDirective} from './auto-grow.directive';
 import {AutoHighlightDirective} from './auto-highlight.directive';
+import {CourseService} from './course.service';
+import {AuthorService} from './author.service';
 
 @NgModule({
   imports:      [ BrowserModule ],
   declarations: [ AppComponent, CoursesComponent, AuthorsComponent, AutoGrowDirective, AutoHighlightDirective ],
+  providers:    [ CourseService, AuthorService],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
