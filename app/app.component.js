45793271d91c714476290b61f94be7eddc0db7ae
Banda <Harish.Banda@FIN.DS.KY.GOV>
2017-03-22 10:03:53 -0400

    nothing important

--- app/app.component.js
+++ app/app.component.js
@@ -5,18 +5,29 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
+var forms_1 = require("@angular/forms");
+require("rxjs/add/operator/debounceTime");
 var AppComponent = (function () {
-    function AppComponent() {
+    function AppComponent(fb) {
+        this.form = fb.group({
+            search: []
+        });
+        var search = this.form.get('search');
+        search.debounceTime(400).map(function (str) { return str.replace(' ', '-'); }).valueChanges.subscribe(function (x) { return console.log(x); });
     }
     return AppComponent;
 }());
 AppComponent = __decorate([
     core_1.Component({
         selector: 'my-app',
-        template: "\n        <change-password></change-password>\n    "
-    })
+        template: "\n        <form [formGroup]=\"form\">\n            <input type=\"text\" formControlName=\"search\">\n        </form>\n    "
+    }),
+    __metadata("design:paramtypes", [forms_1.FormBuilder])
 ], AppComponent);
 exports.AppComponent = AppComponent;
 //# sourceMappingURL=app.component.js.map
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,27 @@
 import {Component} from '@angular/core';
 import {SignUpFormComponent} from './signup-form.component';
 import {FormGroup, FormBuilder ,Validators} from '@angular/forms';
+import {Observable}  from 'rxjs/Observable';
+import 'rxjs/add/operator/debounceTime';
 
 @Component({
     selector: 'my-app',
     template: `
-        <change-password></change-password>
+        <form [formGroup]="form">
+            <input type="text" formControlName="search">
+        </form>
     `
 })
 
 export class AppComponent {
 
+    form:FormGroup
+    constructor(fb:FormBuilder){
+        this.form=fb.group({
+                search:[]
+        })
+
+        var search=this.form.get('search');
+        search.valueChanges.debounceTime(400).map(str=>(<string>str).replace(' ', '-')).subscribe(x=>console.log(x));
+    }
 }
